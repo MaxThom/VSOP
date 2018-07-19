6ffe243db9b7f028d942b45837deb8225a759714
Maxime Thomassin <maxthomassin@hotmail.com>
2018-07-19 10:13:56 -0400

    added main for lexer and parser switch

--- src/main/antlr/Main.java
+++ src/main/antlr/Main.java
@@ -1,7 +1,10 @@
 
 import Lexer.LexerListener;
+import Parser.ParserListener;
 import VSOP.Lexer.LEXERLexer;
 import VSOP.Lexer.LEXERParser;
+import VSOP.Parser.PARSERLexer;
+import VSOP.Parser.PARSERParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
@@ -14,29 +17,49 @@ public class Main {
     public static void main(String[] args) {
         try {
 
-            if (args.length == 0) {
-                System.out.println("Error : Missing input file");
+            if (args.length <= 1) {
+                System.out.println("Error : Missing correct arguments");
                 System.exit(1);
             }
 
             ANTLRInputStream input = new ANTLRInputStream(
-                    new FileInputStream(args[0]));
+                    new FileInputStream(args[1]));
 
-            LEXERLexer lexer = new LEXERLexer(input);
+            switch (args[0]) {
+                case "-lex":
+                    startLexer(input);
+                    break;
+                case "-parse":
+                    startParser(input);
+                    break;
+                default:
+                    System.out.println("Error : Missing correct arguments");
+                    System.exit(1);
+                    break;
+            }
 
-//            List tokens = lexer.getAllTokens();
-//
-//            for (int i = 0; i < tokens.size(); i++) {
-//                System.out.println(tokens.get(i).toString());
-//            }
-
-            LEXERParser parser = new LEXERParser(new CommonTokenStream(lexer));
-            parser.addParseListener(new LexerListener());
-
-            // Start parsing
-            parser.program();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void startLexer(ANTLRInputStream input) {
+        LEXERLexer lexer = new LEXERLexer(input);
+
+        LEXERParser parser = new LEXERParser(new CommonTokenStream(lexer));
+        parser.addParseListener(new LexerListener());
+
+        // Start parsing
+        parser.program();
+    }
+
+    private static void startParser(ANTLRInputStream input) {
+        PARSERLexer lexer = new PARSERLexer(input);
+
+        PARSERParser parser = new PARSERParser(new CommonTokenStream(lexer));
+        parser.addParseListener(new ParserListener());
+
+        // Start parsing
+        parser.program();
     }
 }
